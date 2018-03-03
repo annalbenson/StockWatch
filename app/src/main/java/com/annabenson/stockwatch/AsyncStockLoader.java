4d4085006d92c5e64ba34a2e5aaaaaa13f48dd05
Anna <annalucybenson@gmail.com>
2018-03-03 15:58:00 -0600

    The Third

--- app/src/main/java/com/annabenson/stockwatch/AsyncStockLoader.java
+++ app/src/main/java/com/annabenson/stockwatch/AsyncStockLoader.java
@@ -8,8 +8,8 @@ import java.util.ArrayList;
 /**
  * Created by Anna on 3/3/2018.
  */
-/*
-public class AsyncStockLoader extends AsyncTask<String,Integer,String> {
+
+public class AsyncStockLoader /*extends AsyncTask<String,Integer,String>*/ {
 
     private MainActivity mainActivity;
     private int count;
@@ -17,16 +17,16 @@ public class AsyncStockLoader extends AsyncTask<String,Integer,String> {
     //private final String dataURL = ""
     private static final String TAG = "AysncStockLoader";
 
-    @Override
+    //@Override
     protected void onPreExecute(){
         Toast.makeText(mainActivity, "Loading Stock Data...", Toast.LENGTH_SHORT).show();
     }
 
-    @Override
+    //@Override
     protected void onPostExecute(String s) {
-        ArrayList<Stock> countryList = parseJSON(s);
-        Toast.makeText(mainActivity, "Loaded " + countryList.size() + " countries.", Toast.LENGTH_SHORT).show();
-        mainActivity.updateData(countryList);
+        //ArrayList<Stock> countryList = parseJSON(s);
+        //Toast.makeText(mainActivity, "Loaded " + countryList.size() + " countries.", Toast.LENGTH_SHORT).show();
+        //mainActivity.updateData(countryList);
     }
 
-}*/
+}
